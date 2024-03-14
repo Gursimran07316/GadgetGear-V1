86a75288fec1c7f69fef2d09e271fee50a2f4011
Gursimrandeep Singh Khela <60434846+Gursimran07316@users.noreply.github.com>
2024-03-13 22:23:16 -0400

    Added Product component & homescreen

---
+++ frontend/src/products.js
@@ -0,0 +1,77 @@
+const products = [
+    {
+      name: 'Beats Studio Bluetooth Headphones',
+      image: '/images/beatstudiobus.jpeg',
+      description:
+        'Experience the world of premium sound with Beats Studio Bluetooth Headphones. Enjoy seamless wireless connectivity with Bluetooth technology and high-quality AAC audio for an immersive listening experience. The built-in microphone ensures you can take calls effortlessly while working or on the go.',
+      brand: 'Beats',
+      category: 'Electronics',
+      price: 89.99,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 12,
+    },
+    {
+      name: 'Samsung galaxy s23',
+      image: '/images/galaxys23.jpeg',
+      description:
+        'Discover the Samsung Galaxy S23, featuring a revolutionary triple-camera system for superior photography and videography. Experience a significant leap in battery life and seamless performance for an all-around mobile innovation.',
+      brand: 'Samsung',
+      category: 'Electronics',
+      price: 599.99,
+      countInStock: 7,
+      rating: 4.0,
+      numReviews: 8,
+    },
+    {
+      name: 'Alexa',
+      image: '/images/alexa.webp',
+      description:
+        'Meet Alexa, the brain behind the Amazon Echo Dot. Alexa is designed to make your life easier and more fun with voice control. Now featuring improved sound and a new look, Alexa can play music, answer questions, control smart home devices, and more.',
+      brand: 'Amazon',
+      category: 'Electronics',
+      price: 929.99,
+      countInStock: 5,
+      rating: 3,
+      numReviews: 12,
+    },
+    {
+      name: 'Xbox',
+      image: '/images/xbox.jpeg',
+      description:
+        'Enter the new era of gaming with Xbox, your ultimate home entertainment center. From groundbreaking games to ultra-high-definition movies and more, Xbox brings you an unparalleled entertainment experience that goes beyond gaming.',
+      brand: 'Microsoft',
+      category: 'Electronics',
+      price: 399.99,
+      countInStock: 11,
+      rating: 5,
+      numReviews: 12,
+    },
+    {
+      name: 'Ninetendo Switch',
+      image: '/images/swithNT.jpeg',
+      description:
+        'Dive into a flexible gaming experience with the Ninetendo Switch. Play at home on the TV or on-the-go with a vibrant 7-inch OLED screen. The Switch offers various play modes and includes Joy-Con controllers for single or multiplayer fun.',
+      brand: 'Nintendo',
+      category: 'Electronics',
+      price: 49.99,
+      countInStock: 7,
+      rating: 3.5,
+      numReviews: 10,
+    },
+    {
+      name: 'JBL flip 5',
+      image: '/images/jblflip5.jpeg',
+      description:
+        'Bring your music to life wherever you go with the JBL Flip 5, our most popular portable speaker. Enjoy booming bass in a compact package, perfect for any space or adventure. With its waterproof design, the Flip 5 is ready for any weather or pool party.',
+      brand: 'JBL',
+      category: 'Electronics',
+      price: 29.99,
+      countInStock: 0,
+      rating: 4,
+      numReviews: 12,
+    },
+  ];
+  
+  export default products;
+  
