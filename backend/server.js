8cd8df2cb748afe6feb927a88e76a11911203b24
Gursimrandeep Singh Khela <60434846+Gursimran07316@users.noreply.github.com>
2024-03-15 21:40:51 -0400

    Implemented Fetching of products from the backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import products from './data/products.js';
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 const app = express();
 
