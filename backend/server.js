be20eb9d22186ed59209342be639ace132079f97
Gursimrandeep Singh Khela <60434846+Gursimran07316@users.noreply.github.com>
2024-03-18 19:10:06 -0400

    Implmented error middleware and product controller

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
@@ -8,7 +9,8 @@ connectDB();
 const app = express();
 
 app.use('/api/products', productRoutes);
-
+app.use(notFound);
+app.use(errorHandler);
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
