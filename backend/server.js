2340dd49be30faa00fd0b387d17eef044e1efc54
Gursimrandeep Singh Khela <60434846+Gursimran07316@users.noreply.github.com>
2024-03-18 18:14:22 -0400

    implemented Database connection & model data

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import products from './data/products.js';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
 const port = process.env.PORT || 5001;
-
+connectDB();
 const app = express();
 
 app.get('/api/products', (req, res) => {
@@ -17,4 +20,5 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
