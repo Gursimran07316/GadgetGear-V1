535af7eb170535676c41140b280107fcd19d3b88
Gursimrandeep Singh Khela <60434846+Gursimran07316@users.noreply.github.com>
2024-03-27 22:41:39 -0400

    Implemented jwt setup and user login and login routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
@@ -8,11 +9,13 @@ import userRoutes from './routes/userRoutes.js';
 const port = process.env.PORT || 5001;
 connectDB();
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
+app.use(cookieParser)
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
