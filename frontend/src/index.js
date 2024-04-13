44baf5b2b4fd366d90feda6a7ec01aa6d988dcae
Gursimrandeep Singh Khela <60434846+Gursimran07316@users.noreply.github.com>
2024-04-12 23:14:27 -0400

    created order page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,6 +21,7 @@ import ShippingScreen from './screens/ShippingScreen';
 import PrivateRoute from './components/PrivateRoute';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceHolderScreen';
+import OrderScreen from './screens/OrderApiScreen';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
@@ -34,6 +35,7 @@ const router = createBrowserRouter(
         <Route path='/shipping' element={<ShippingScreen />} />
         <Route path='/payment' element={<PaymentScreen />} />
         <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route path='/order/:id' element={<OrderScreen />} />
       </Route>
     </Route>
   )
